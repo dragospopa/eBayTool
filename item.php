5882a1489d20ddf5148cb9ff907368b110d19e8e
Daniel Savu <dani@Daniels-MacBook-Pro.local>
2019-03-11 01:11:04 +0000

    banii il lasati la preot va rog nu la mine

--- item.php
+++ item.php
@@ -28,11 +28,11 @@
                     <div class="sidebar-brand-icon"> <i class="fas fa-tags"></i> </div>
                     <div class="sidebar-brand-text mx-3">eBay Auctioneer</div>
                 </a>
-                <form action="search.html" method="get" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <form action="search.html"  onclick="if(event.target.id === \'search-btn\') this.submit()" method="get" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                     <div class="input-group">
                         <input id="search-txt" name="search" type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                         <div class="input-group-append">
-                            <button class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
+                            <button id="search-btn" class="btn btn-primary" type="button"> <i class="fas fa-search fa-sm"></i> </button>
                         </div>
                     </div>
                 </form>
