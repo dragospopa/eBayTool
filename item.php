1570c1a8fdff0624a1a9e3255742c952dcb4b995
dragospopa <dragos.popa21@yahoo.com>
2019-03-11 01:28:06 +0000

    changed titles of all pages

--- item.php
+++ item.php
@@ -142,7 +142,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>eBay Auctioneer</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
